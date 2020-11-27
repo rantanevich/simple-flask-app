16c7798e30b1b0bf460aafb1fc686ce1c4d22cc0
Raman Antanevich <r.antanevich@ya.ru>
2020-11-27 18:02:50 +0300

    test multibranch pipeline

--- server.py
+++ server.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     time = datetime.now().ctime()
-    return f'<h1><b>Time in Minsk:</b> {time}</h1>'
+    return f'<p><b>Time in Minsk:</b> {time}</p>'
